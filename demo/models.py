d00c3fb5cc60c5aeec2afa1d09974223b1cd43b9
anshuman910 <anshumansaha99@yahoo.com>
2020-05-10 22:59:14 +0530

    Initial commit

---
+++ demo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=36, blank=False, unique=True)
+    description = models.TextField(max_length=256, blank=True)
+
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=5)
+    published = models.DateField(blank=True, null=True, default=None)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='Covers/', blank=True)
