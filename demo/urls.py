0be68db86d0fe1c718c796561c338ef9db207872
anshuman910 <anshumansaha99@yahoo.com>
2020-05-12 09:46:57 +0530

    pycharm

--- demo/urls.py
+++ demo/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from .views import Another
+# from .views import Another
 
 urlpatterns = [
     path('first', views.first),
-    path('Another',Another.as_view()),
+    #path('Another', Another.as_view()),
 
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views import View
-from .models import Book
+
+from django.shortcuts import render
+
+
+# from django.views import View
+# from .models import Book
 
 
 # Create your views here.
-class Another(View):
-    books = Book.objects.all()
-    output = f"We have {len(books)} books in database"
-    names = ''
-    for book in books:
-        names += f"We have \" {book.title} \" book in database <br> "
-    print(names)
+# class Another(View):
+#     books = Book.objects.all()
+#     output = f"We have {len(books)} books in database"
+#     names = ''
+#     for book in books:
+#         names += f"We have \" {book.title} \" book in database <br> "
+#     print(names)
+#
+#     def get(self, request):
+#         return HttpResponse(self.output)
 
-    def get(self, request):
-        return HttpResponse(self.output)
 
+# def first(request):
+#     return HttpResponse('First message from views')
 
 def first(request):
-    return HttpResponse('First message from views')
+    return render(request, 'first_temp.html')
