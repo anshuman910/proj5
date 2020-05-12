a1b94f31ac8567f37ed03a17b3a1916d3c3f0a24
anshuman910 <anshumansaha99@yahoo.com>
2020-05-12 20:00:29 +0530

    pycharm

--- demo/views.py
+++ demo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-from django.shortcuts import render
+
+# from django.http import HttpResponse
 
 
 # from django.views import View
