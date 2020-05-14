5b110d7286c25bbedaa519ffb4c80860fab95512
anshuman910 <anshumansaha99@yahoo.com>
2020-05-14 14:18:26 +0530

    14.5.2020 v2

--- demo/admin.py
+++ demo/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Book
+
+
 # Register your models here.
-#admin.site.register(Book)
+# admin.site.register(Book)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     fields = ['title', 'description', 'price', 'published', 'is_published', 'cover', 'pages']
+    list_display = ['title', 'description']
+    list_filter = ['published']
+    search_fields = ['title']
+
